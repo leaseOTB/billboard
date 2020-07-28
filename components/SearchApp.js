407ecb0b069b43d1297646d50b56d7f845737e45
Joshua Grossman <Josh1794@gmail.com>
2020-07-28 15:47:12 -0400

    fix:new card styling, new card component

--- components/SearchApp.js
+++ components/SearchApp.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 
-import Link from 'next/link'
-import PropTypes from 'prop-types'
-
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+import SearchCard from './SearchCard';
 import {
   RefinementList,
   connectSearchBox,
@@ -12,94 +12,95 @@ import {
   connectPagination,
   Highlight,
   InstantSearch,
-} from 'react-instantsearch-dom'
+} from 'react-instantsearch-dom';
 
 import {
-  Grid, 
-  Card, 
-  Typography, 
-  CardContent, 
-  CardActionArea, 
-  CircularProgress, 
+  Grid,
+  Card,
+  Typography,
+  CardContent,
+  CardActionArea,
+  CircularProgress,
   CardMedia,
   Button,
-  AppBar, 
+  AppBar,
   Toolbar,
   IconButton,
   TextField,
   Paper,
   Chip,
-} from '@material-ui/core'
+} from '@material-ui/core';
 
-import {Alert, AlertTitle} from '@material-ui/lab'
-import ClearIcon from '@material-ui/icons/Clear'
-import SearchIcon from '@material-ui/icons/Search'
+import { Alert, AlertTitle } from '@material-ui/lab';
+import ClearIcon from '@material-ui/icons/Clear';
+import SearchIcon from '@material-ui/icons/Search';
 
-let searching = false
+let searching = false;
 
 const SearchBox = ({ currentRefinement, isSearchStalled, refine }) => {
-  if (currentRefinement !== null) {searching = true}
-  if (currentRefinement == '') {searching = false}
+  if (currentRefinement !== null) {
+    searching = true;
+  }
+  if (currentRefinement == '') {
+    searching = false;
+  }
   return (
     <Grid container direction='row'>
-      <form noValidate action="" role='search' style={{width: '70%', marginTop: '2em',}}>
-        <TextField 
+      <form
+        noValidate
+        action=''
+        role='search'
+        style={{ width: '70%', marginTop: '2em' }}
+      >
+        <TextField
           fullWidth
           autoFocus
           type='search'
           size='medium'
           placeholder={'Search by Address, ZIP Code, etc...'}
-          value={currentRefinement} 
-          onChange={event => refine(event.currentTarget.value)} />
+          value={currentRefinement}
+          onChange={(event) => refine(event.currentTarget.value)}
+        />
       </form>
     </Grid>
-  )
-}
+  );
+};
 
-const CustomSearchBox = connectSearchBox(SearchBox)
+const CustomSearchBox = connectSearchBox(SearchBox);
 
 const Hits = ({ hits }) => {
-
-  if (hits.length === 0 || !hits) return (
-    <Alert severity="error">
-      <AlertTitle>No results... Try something else!</AlertTitle>
-    </Alert>
-  )
+  if (hits.length === 0 || !hits)
+    return (
+      <Alert severity='error'>
+        <AlertTitle>No results... Try something else!</AlertTitle>
+      </Alert>
+    );
   return (
-      <Grid container direction='row' spacing={2}>
-        {hits.map(hit => (
-          <Grid item xs={12} md={4} key={hit.objectID}>
-            <Card style={{minHeight: '5em', backgroundColor: 'none'}}>
-              <CardActionArea href={`/${hit._id}`} style={{padding: '.5em'}}>
-                <Grid container direction='row'>
-                  <Grid item xs={5}>
-                    <img style={{maxHeight: '7em', }} src={`https://maps.googleapis.com/maps/api/streetview?location=${hit.STREET_ADDRESS}&size=800x800&key=${process.env.GOOGLE_API}`} />
-                  </Grid>
-                  <Grid item xs={7}>
-                    <Typography variant='body1'>
-                    {hit.STREET_ADDRESS}
-                    </Typography>
-                    <Typography variant='caption'>
-                    {hit.ZIP}
-                    </Typography>
-                    <br/>
-                    <br/>
-                  </Grid>
-                </Grid>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-  )
-}
+    <Grid container direction='row' spacing={2}>
+      {hits.map((hit) => (
+        <SearchCard
+          address={hit.STREET_ADDRESS}
+          zipCode={hit.ZIP}
+          key={hit.objectID}
+          id={hit._id}
+        />
+      ))}
+    </Grid>
+  );
+};
 
-const CustomHits = connectInfiniteHits(Hits)
+const CustomHits = connectInfiniteHits(Hits);
 
-
-const SearchApp = ({searchState, resultsState, onSearchStateChange, createURL, indexName, searchClient }) => {
+const SearchApp = ({
+  searchState,
+  resultsState,
+  onSearchStateChange,
+  createURL,
+  indexName,
+  searchClient,
+}) => {
   return (
-    <div style={{minHeight: '90vh'}}>
+    <div style={{ minHeight: '90vh' }}>
       <InstantSearch
         searchClient={searchClient}
         resultsState={resultsState}
@@ -107,30 +108,34 @@ const SearchApp = ({searchState, resultsState, onSearchStateChange, createURL, i
         searchState={searchState}
         createURL={createURL}
         indexName={indexName}
-        > 
-          <br/>
-          <Typography variant='h4' color='textPrimary'>Affordable Housing Community Billboard</Typography>
-          <CustomSearchBox />
-          <br/>
-            {!searching ? <>
-              <br/>
-              <Alert severity="info">
-                <AlertTitle>Start Typing to Find Your Building!</AlertTitle>
-              </Alert>
-              <br/>
+      >
+        <br />
+        <Typography variant='h4' color='textPrimary'>
+          Affordable Housing Community Billboard
+        </Typography>
+        <CustomSearchBox />
+        <br />
+        {!searching ? (
+          <>
+            <br />
+            <Alert severity='info'>
+              <AlertTitle>Start Typing to Find Your Building!</AlertTitle>
+            </Alert>
+            <br />
 
-              <CustomHits />
-
-            </> : <>
-          
+            <CustomHits />
+          </>
+        ) : (
+          <>
             <CustomHits />
 
-            <br/>
-            </>}
+            <br />
+          </>
+        )}
       </InstantSearch>
     </div>
-  )
-}
+  );
+};
 
 SearchApp.propTypes = {
   seachState: PropTypes.object,
@@ -138,7 +143,7 @@ SearchApp.propTypes = {
   onSearchStateChange: PropTypes.func,
   createURL: PropTypes.func,
   indexName: PropTypes.string,
-  searchClient: PropTypes.object
-}
+  searchClient: PropTypes.object,
+};
 
-export default SearchApp
+export default SearchApp;
---
+++ components/SearchCard.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+import {
+  Grid,
+  Card,
+  Typography,
+  CardContent,
+  CardActionArea,
+  CircularProgress,
+  CardMedia,
+  Button,
+  AppBar,
+  Toolbar,
+  IconButton,
+  TextField,
+  Paper,
+  Chip,
+} from '@material-ui/core';
+
+export default function SearchCard(props) {
+  return (
+    <Grid item xs={12} md={3}>
+      <Card
+        style={{
+          minHeight: '4em',
+          maxWidth: '30em',
+          backgroundColor: 'none',
+        }}
+      >
+        <CardActionArea
+          href={`/${props.id}`}
+          style={{
+            padding: '.5em',
+          }}
+        >
+          <Grid
+            container
+            direction='row'
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Grid
+              item
+              xs={5}
+              style={{
+                maxWidth: '7em',
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <img
+                style={{
+                  maxHeight: '7em',
+                  padding: '.2em',
+                }}
+                src={`https://maps.googleapis.com/maps/api/streetview?location=${props.address}&size=800x800&key=${process.env.GOOGLE_API}`}
+              />
+            </Grid>
+            <Grid
+              container
+              direction='row'
+              item
+              xs={7}
+              justify='center'
+              alignItems='center'
+            >
+              <Typography variant='body1'>{props.address}</Typography>
+              <br />
+              <br />
+            </Grid>
+          </Grid>
+        </CardActionArea>
+      </Card>
+    </Grid>
+  );
+}
