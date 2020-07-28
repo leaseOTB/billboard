55ceb52a8d3dd32487afafdc178dbd1ebd6957fa
Joshua Grossman <Josh1794@gmail.com>
2020-07-28 18:42:40 -0400

    fix:added basic unstyled list view

--- components/ListHits.js
+++ components/ListHits.js
@@ -24,6 +24,14 @@ import {
   TextField,
   Paper,
   Chip,
+  Table,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableContainer,
+  withStyles,
+  makeStyles,
+  TableCell,
 } from '@material-ui/core';
 import { Alert, AlertTitle } from '@material-ui/lab';
 import SearchList from './SearchList';
@@ -35,17 +43,53 @@ const ListHits = ({ hits }) => {
         <AlertTitle>No results... Try something else!</AlertTitle>
       </Alert>
     );
+  const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+
+  const StyledTableRow = withStyles((theme) => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }))(TableRow);
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 700,
+    },
+  });
+  const classes = useStyles();
 
   return (
     <Grid container direction='row' spacing={2}>
-      {hits.map((hit) => (
-        <SearchList
-          address={hit.STREET_ADDRESS}
-          zipCode={hit.ZIP}
-          key={hit.objectID}
-          id={hit._id}
-        />
-      ))}
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label='customized table'>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Picture</StyledTableCell>
+              <StyledTableCell align='right'>Address</StyledTableCell>
+              <StyledTableCell align='riht'>Zip Code</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {hits.map((hit) => (
+              <SearchList
+                address={hit.STREET_ADDRESS}
+                zipCode={hit.ZIP}
+                key={hit.objectID}
+                id={hit._id}
+              />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Grid>
   );
 };
--- components/SearchList.js
+++ components/SearchList.js
@@ -3,81 +3,117 @@ import Link from 'next/link';
 import PropTypes from 'prop-types';
 import {
   Grid,
-  Card,
-  Typography,
-  CardContent,
-  CardActionArea,
-  CircularProgress,
-  CardMedia,
-  Button,
-  AppBar,
-  Toolbar,
-  IconButton,
-  TextField,
   Paper,
-  Chip,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  withStyles,
+  makeStyles,
 } from '@material-ui/core';
 
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
 export default function SearchList(props) {
+  const classes = useStyles();
+
   return (
-    <Grid item xs={12} md={3}>
-      <Card
-        style={{
-          minHeight: '4em',
-          maxWidth: '30em',
-          backgroundColor: 'purple',
-        }}
-      >
-        <CardActionArea
-          href={`/${props.id}`}
-          style={{
-            padding: '.5em',
-          }}
-        >
-          <Grid
-            container
-            direction='row'
+    // <Grid item xs={12} md={3}>
+    //   <Card
+    //     style={{
+    //       minHeight: '4em',
+    //       maxWidth: '30em',
+    //       backgroundColor: 'purple',
+    //     }}
+    //   >
+    //     <CardActionArea
+    //       href={`/${props.id}`}
+    //       style={{
+    //         padding: '.5em',
+    //       }}
+    //     >
+    //       <Grid
+    //         container
+    //         direction='row'
+    //         style={{
+    //           display: 'flex',
+    //           flexDirection: 'row',
+    //           justifyContent: 'center',
+    //           alignItems: 'center',
+    //         }}
+    //       >
+    //         <Grid
+    //           item
+    //           xs={5}
+    //           style={{
+    //             maxWidth: '7em',
+    //             display: 'flex',
+    //             flexDirection: 'row',
+    //             justifyContent: 'center',
+    //             alignItems: 'center',
+    //           }}
+    //         >
+    //           <img
+    //             style={{
+    //               maxHeight: '7em',
+    //               padding: '.2em',
+    //             }}
+    //             src={`https://maps.googleapis.com/maps/api/streetview?location=${props.address}&size=800x800&key=${process.env.GOOGLE_API}`}
+    //           />
+    //         </Grid>
+    //         <Grid
+    //           container
+    //           direction='row'
+    //           item
+    //           xs={7}
+    //           justify='center'
+    //           alignItems='center'
+    //         >
+    //           <Typography variant='body1'>{props.address}</Typography>
+    //           <br />
+    //           <br />
+    //         </Grid>
+    //       </Grid>
+    //     </CardActionArea>
+    //   </Card>
+    // </Grid>
+    <StyledTableRow key={props.id}>
+      <a href={`/${props.id}`}>
+        <StyledTableCell component='th' scope='row'>
+          <img
             style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
+              maxHeight: '7em',
+              padding: '.2em',
             }}
-          >
-            <Grid
-              item
-              xs={5}
-              style={{
-                maxWidth: '7em',
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              <img
-                style={{
-                  maxHeight: '7em',
-                  padding: '.2em',
-                }}
-                src={`https://maps.googleapis.com/maps/api/streetview?location=${props.address}&size=800x800&key=${process.env.GOOGLE_API}`}
-              />
-            </Grid>
-            <Grid
-              container
-              direction='row'
-              item
-              xs={7}
-              justify='center'
-              alignItems='center'
-            >
-              <Typography variant='body1'>{props.address}</Typography>
-              <br />
-              <br />
-            </Grid>
-          </Grid>
-        </CardActionArea>
-      </Card>
-    </Grid>
+            src={`https://maps.googleapis.com/maps/api/streetview?location=${props.address}&size=800x800&key=${process.env.GOOGLE_API}`}
+          />
+        </StyledTableCell>
+        <StyledTableCell align='right'>{props.address}</StyledTableCell>
+        <StyledTableCell align='right'>{props.zipCode}</StyledTableCell>
+      </a>
+    </StyledTableRow>
   );
 }
